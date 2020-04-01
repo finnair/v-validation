d6c9dc82d801ca1ede7c44af0620a37de4058ffd
Samppa Saarela <samppa.saarela@iki.fi>
2020-04-01 15:13:22 +0300

    Check if required GH_TOKEN env variable exists
    Signed-off-by: Samppa Saarela <samppa.saarela@iki.fi>

--- publish.sh
+++ publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
+if [[ -z "${GH_TOKEN}" ]]; then
+  echo 'Error GH_TOKEN env variable missing'
+  exit 1;
+fi
+
 lerna version ${1:-minor} --conventional-commits --create-release github
 lerna publish from-package
