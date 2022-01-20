84b35ca281b74d5093c050d79baabcc2aa62ffde
Samppa Saarela <samppa.saarela@iki.fi>
2022-01-20 14:26:40 +0200

    Fix publish.sh - use local lerna instead of global

--- publish.sh
+++ publish.sh
@@ -9,10 +9,11 @@ fi
 set -v
 
 yarn clean
+yarn install
 yarn boot
 yarn build
 yarn test --coverage
 npm config set access public
 npm login
-lerna version ${1:-minor} --conventional-commits --create-release github
-lerna publish from-package
+npx lerna version ${1:-minor} --conventional-commits --create-release github
+npx lerna publish from-package
