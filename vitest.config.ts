a0414588ab7fc6adac40a10840effc9e7cbec19c
Samppa Saarela <samppa.saarela@iki.fi>
2025-09-12 12:38:23 +0300

    chore: update dependencies
    Signed-off-by: Samppa Saarela <samppa.saarela@iki.fi>

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   test: {
     coverage: {
       enabled: true,
-      exclude: ['./*.ts', './*.cjs', 'test/**/*', 'ci/**/*'],
+      include: ['**/*.ts'],
+      exclude: ['**/*.d.ts', '**/*.spec.ts', '**/*.test.ts', 'vitest.config.ts'],
       provider: 'v8',
       reporter: [['lcov'], ['text'], ['text-summary']],
       reportsDirectory: './coverage',
